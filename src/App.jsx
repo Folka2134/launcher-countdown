415af27d6efeb8ceccc63ef40aebd0abc305ee4d
Folka2134 <folka2134@gmail.com>
2023-04-11 14:22:29 +0100

    finished background and hard-coded timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
+import backgroundStars from "./assets/images/bg-stars.svg";
+import backgroundHills from "./assets/images/pattern-hills.svg";
 
 function App() {
   return (
-    <div className="App">
-      <div className="bg-purple-400">test</div>
+    <div className="App h-screen bg-gradient-to-b from-[#191A24] via-[#1E1F29] to-[#343650] flex justify-center font-red-hat">
+      {/* <img src={backgroundStars} alt="" /> */}
+      <div className="bg-pattern-stars h-full bg-no-repeat w-full absolute "></div>
+      <div className="bg-pattern-hill h-48 bg-no-repeat bg-cover w-full bottom-0 absolute"></div>
+      <div className="p-6 mt-48 md:mt-72">
+        <h1 className="text-3xl text-center text-white uppercase tracking-widest ">
+          We're Launching Soon
+        </h1>
+        <div className="mt-32 flex justify-evenly text-center">
+          <div className="flex flex-col">
+            <h1 className="text-7xl text-[#FB6087]">08</h1>
+            <h6 className="text-sm text-[#8486A9] uppercase mt-3">Days</h6>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-7xl text-[#FB6087]">08</h1>
+            <h6 className="text-sm text-[#8486A9] uppercase mt-3">Hours</h6>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-7xl text-[#FB6087]">08</h1>
+            <h6 className="text-sm text-[#8486A9] uppercase mt-3">Minutes</h6>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-7xl text-[#FB6087]">08</h1>
+            <h6 className="text-sm text-[#8486A9] uppercase mt-3">Seconds</h6>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "pattern-hill": "url('/src/assets/images/pattern-hills.svg')",
+        "pattern-stars": "url('/src/assets/images/bg-stars.svg')",
+      },
+      fontFamily: {
+        "red-hat": ['"Red Hat Text"'],
+      },
+    },
   },
   plugins: [],
 };
